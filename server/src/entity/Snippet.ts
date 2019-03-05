6071d2df0adc40b7464cc06ca771fad862c34eea
Suharsh Nagasampagi <suharsh329@gmail.com>
2019-03-05 22:35:04 +0530

    Add snippet model with many to one relationship with User

---
+++ server/src/entity/Snippet.ts
@@ -0,0 +1,43 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm";
+import { User } from "./User";
+
+@Entity({ name: "snippets" })
+export class Snippet {
+
+  @PrimaryGeneratedColumn()
+  sid: number;
+
+  @Column({
+    type: "text",
+    nullable: false
+  })
+  title: string;
+
+  @Column({
+    type: "text",
+    nullable: false,
+  })
+  code: string;
+
+  @Column({
+    type: "text",
+    nullable: false,
+  })
+  language: string;
+
+  @Column({
+    type: "simple-array",
+    nullable: false
+  })
+  tags: string[];
+
+  @Column({
+    type: "timestamp with time zone",
+    default: () => "CURRENT_TIMESTAMP"
+  })
+  createdAt: string;
+
+  @ManyToOne(type => User, user => user.uid, { eager: true })
+  user: User
+
+}
