e480fe4d8d8bdd2f08ca092496803a85ea62cdad
Suharsh <suharsh329@gmail.com>
2019-11-04 18:04:38 +1100

    Correct the tag numbers

--- server/sql/snippets_tags.sql
+++ server/sql/snippets_tags.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS snippets_tags(id SERIAL PRIMARY KEY, sid INT REFERENCES snippets(sid), tid INT REFERENCES tags(tid));
 
-INSERT INTO snippets_tags(sid, tid) VALUES(1, 23);
-INSERT INTO snippets_tags(sid, tid) VALUES(1, 25);
-INSERT INTO snippets_tags(sid, tid) VALUES(1, 29);
-INSERT INTO snippets_tags(sid, tid) VALUES(2, 21);
-INSERT INTO snippets_tags(sid, tid) VALUES(3, 18);
-INSERT INTO snippets_tags(sid, tid) VALUES(3, 33);
-INSERT INTO snippets_tags(sid, tid) VALUES(4, 19);
+INSERT INTO snippets_tags(sid, tid) VALUES(1, 14);
+INSERT INTO snippets_tags(sid, tid) VALUES(1, 10);
+INSERT INTO snippets_tags(sid, tid) VALUES(1, 21);
+INSERT INTO snippets_tags(sid, tid) VALUES(2, 19);
+INSERT INTO snippets_tags(sid, tid) VALUES(3, 16);
+INSERT INTO snippets_tags(sid, tid) VALUES(3, 31);
+INSERT INTO snippets_tags(sid, tid) VALUES(4, 17);
 INSERT INTO snippets_tags(sid, tid) VALUES(4, 22);
 INSERT INTO snippets_tags(sid, tid) VALUES(4, 34);
